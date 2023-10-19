5617bce8495c1a9e42ba3204d5895dd5841742a6
Fizruk02 <h10189515@gmail.com>
2023-10-19 18:15:29 +0300

    player selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 const gameBoard = document.getElementById('game-board');
 const cells = [];
 
+let currentPlayer = null;
+
+function selectPlayer(playerSymbol) {
+    if (!currentPlayer) {
+        currentPlayer = playerSymbol;
+        document.getElementById('player-selection').style.display = 'none';
+    } else {
+        alert('Игрок уже выбран.');
+    }
+}
+
 for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
         const cell = document.createElement('div');
@@ -9,16 +20,15 @@ for (let row = 0; row < 3; row++) {
         cell.dataset.row = row;
         cell.dataset.col = col;
         cells.push(cell);
+        cell.addEventListener('click', makeMove);
         gameBoard.appendChild(cell);
     }
 }
 
-let currentPlayer = 'X';
-
-gameBoard.addEventListener('click', (event) => {
+function makeMove(event) {
     const cell = event.target;
-    if (cell.classList.contains('cell') && !cell.textContent) {
+    if (cell.classList.contains('cell') && !cell.textContent && currentPlayer) {
         cell.textContent = currentPlayer;
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     }
-});
+}
