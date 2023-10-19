adf39b5372dc90bfc0f142c7db4d8957258b1546
Fizruk02 <h10189515@gmail.com>
2023-10-19 18:26:51 +0300

    check winner function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 const gameBoard = document.getElementById('game-board');
 const cells = [];
+const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
 
 let currentPlayer = null;
+let gameOver = false;
 
 function selectPlayer(playerSymbol) {
     if (!currentPlayer) {
@@ -27,8 +33,24 @@ for (let row = 0; row < 3; row++) {
 
 function makeMove(event) {
     const cell = event.target;
-    if (cell.classList.contains('cell') && !cell.textContent && currentPlayer) {
+    if (cell.classList.contains('cell') && !cell.textContent && currentPlayer && !gameOver) {
         cell.textContent = currentPlayer;
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        const winner = checkWinner();
+        if (winner) {
+            gameOver = true;
+            alert(`Игрок ${winner} выиграл!`);
+        }
     }
+}
+
+function checkWinner() {
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[c].textContent) {
+            return cells[a].textContent;
+        }
+    }
+    return '';
+    
 }
